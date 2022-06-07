41ee6f70cc7f4e7d7e1e33212ec6e8513a5cf22d
lhx <119414860@qq.com>
2022-06-07 14:38:41 +0800

    feat: 完成作业

--- src/plugins/copyright-webpack-plugin.js
+++ src/plugins/copyright-webpack-plugin.js
@@ -1,9 +1,11 @@
 class CopyrightWebpackPlugin {
     constructor (options) {
-      console.log('插件被使用了')
-      console.log(options)
+      console.log(`恭喜: 作者是${options.auth}的插件被您使用了! `)
     }
     apply (compiler) {
+        compiler.hooks.compile.tap('CopyrightWebpackPlugin', compilation => {
+            console.log('compileHooks')
+        })
         compiler.hooks.emit.tapAsync('CopyrightWebpackPlugin', (compilation, callback) => {
             console.log('生成资源到 output 目录之前')
             compilation.assets['Copyright.txt'] = {
